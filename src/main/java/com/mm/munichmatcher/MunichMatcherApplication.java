7181845adf36dc74358e0157e1094911806e834d
Magnus <magnus.voss@tum.de>
2024-11-22 22:49:25 +0100

    first push

---
+++ src/main/java/com/mm/munichmatcher/MunichMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.mm.munichmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunichMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunichMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mm/munichmatcher/MunichMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.munichmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunichMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
