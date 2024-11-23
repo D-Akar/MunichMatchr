ac5be5a6c2228fc16045bf53da4b46fe11e41ac4
0000000001367D85 <floriankorn93@web.de>
2024-11-23 11:13:37 +0100

    Worked on data fields and implementation for restfull in Organization.

--- src/main/java/com/mm/munichmatcher/Organisation/Organisation.java
+++ src/main/java/com/mm/munichmatcher/Organisation/Organisation.java
@@ -1,19 +1,93 @@
 package com.mm.munichmatcher.Organisation;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+@Document(collection = "Organisation")
 public class Organisation {
 
     @Id
-    private Long id;
+    private String id;
+    private String email;
+    private String contactPerson;
+    private String description;
+    private String phoneNumber;
+    private String website;
+    private String address;
+    private long longitude;
 
-    public void setId(Long id) {
+    public Organisation(long longitude, String address, String website, String phoneNumber, String description, String contactPerson, String email, String id) {
+        this.longitude = longitude;
+        this.address = address;
+        this.website = website;
+        this.phoneNumber = phoneNumber;
+        this.description = description;
+        this.contactPerson = contactPerson;
+        this.email = email;
         this.id = id;
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
     }
 }
--- src/main/java/com/mm/munichmatcher/Organisation/OrganisationService.java
+++ src/main/java/com/mm/munichmatcher/Organisation/OrganisationService.java
@@ -1,7 +1,10 @@
 package com.mm.munichmatcher.Organisation;
 
+import org.hibernate.jdbc.Expectation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganisationService {
@@ -9,5 +12,19 @@ public class OrganisationService {
     @Autowired
     private OrganisationRepository organisationRepository;
 
+    public List<Organisation> getAllOrgas() {
+        return organisationRepository.findAll();
+    }
 
+    public Organisation getOrgaByEmail(String email) {
+        return organisationRepository.findById(email).orElse(null);
+    }
+
+    public Organisation addOrga(Organisation orga) {
+        return organisationRepository.save(orga);
+    }
+
+    public void deleteUser(String email) {
+        organisationRepository.deleteByEmail(email);
+    }
 }
