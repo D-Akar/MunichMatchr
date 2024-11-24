74022cf0220ef5de42ada1425df062b415764d92
Magnus <magnus.voss@tum.de>
2024-11-24 08:36:50 +0100

    getFilteredEvents

--- backend/src/main/java/com/mm/munichmatcher/Event/EventService.java
+++ backend/src/main/java/com/mm/munichmatcher/Event/EventService.java
@@ -21,7 +21,7 @@ public class EventService {
         List<String> interests = user.getInterests();
         List<String> userLanguagues = user.getLanguages();
         List<String> userPrefTypes = user.getPreferredTypesOfEvents();
-        return getAllEvents().stream().filter(event -> event.getInterests().stream().anyMatch(interests::contains) && event.getLanguages().stream().anyMatch(userLanguagues::contains) && userPrefTypes.stream().anyMatch(List.of(event.getTypeOfEvent())::contains)).toList();
+        return getAllEvents().stream().filter(event -> event.getInterests().stream().anyMatch(interests::contains) && event.getLanguages().stream().anyMatch(userLanguagues::contains) && List.of(event.getTypeOfEvent()).stream().anyMatch(userPrefTypes::contains)).toList();
     }
 
     public Event addEvent(Event event) {
